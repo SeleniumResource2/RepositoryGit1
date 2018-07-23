4b7fa1082c0a3a888a9c645358f54992f5dff1f7
SeleniumResource2 <selenium.resource2@gmail.com>
2018-07-23 19:12:22 +0530

    Adding access file to framework

---
+++ src/TestPack/AccessBrowser.java
@@ -0,0 +1,13 @@
+package TestPack;
+
+public class AccessBrowser {
+	
+	public static void main(String[] args) {
+		Access();
+	}
+	
+	public static void Access(){
+		System.out.println("Five Browser to be validated for compatibility");
+	}
+
+}
