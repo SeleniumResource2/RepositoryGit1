2542b339230e93eb612bf6e990fd3097d5537c64
SeleniumResource2 <selenium.resource2@gmail.com>
2018-07-23 18:15:58 +0530

    Framework changes were done

---
+++ src/TestPack/FrameWork.java
@@ -0,0 +1,13 @@
+package TestPack;
+
+public class FrameWork {
+	
+	public static void main(String[] args) {
+		myFrameWorkComponents();
+	}
+	
+	public static void myFrameWorkComponents(){
+		System.out.println("This is my Testing Framework with GitHub");
+	}
+
+}
