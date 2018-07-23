bcba29221fb3d817534291d5ed93bbcff90ddb9e
SeleniumResource2 <selenium.resource2@gmail.com>
2018-07-23 18:52:46 +0530

    addition of code...

---
+++ src/TestPack/SampleFmwk.java
@@ -0,0 +1,12 @@
+package TestPack;
+
+public class SampleFmwk {
+	
+ public static void Addition(){
+	 System.out.println("Addition in git hub");
+ }
+ 
+ public static void main(String[] args) {
+	Addition();
+}
+}
