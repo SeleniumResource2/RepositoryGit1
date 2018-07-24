a7590bd5b71e5b44e595afe1d57645b97e62e613
SeleniumResource2 <selenium.resource2@gmail.com>
2018-07-24 22:28:01 +0530

    updated files

--- src/TestPack/TestCase1.java
+++ src/TestPack/TestCase1.java
@@ -4,6 +4,7 @@ public class TestCase1 {
 	
 	public static void testcaseone(){
 		System.out.println("Adding files in git based on test case name");
+		System.out.println("updated file");
 	}
 	
 	public static void main(String[] args) {
