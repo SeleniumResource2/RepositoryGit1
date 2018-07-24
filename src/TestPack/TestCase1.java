2f4bdf52f3c1d34b923719956502709e2527bafb
SeleniumResource2 <selenium.resource2@gmail.com>
2018-07-24 22:12:55 +0530

    added new test case

---
+++ src/TestPack/TestCase1.java
@@ -0,0 +1,13 @@
+package TestPack;
+
+public class TestCase1 {
+	
+	public static void testcaseone(){
+		System.out.println("Adding files in git based on test case name");
+	}
+	
+	public static void main(String[] args) {
+		testcaseone();
+	}
+
+}
