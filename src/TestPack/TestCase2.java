2908a6bb7c2f4b8be4ddc16d4e580dba7baabf45
SeleniumResource2 <selenium.resource2@gmail.com>
2018-07-24 23:17:43 +0530

    New feature

---
+++ src/TestPack/TestCase2.java
@@ -0,0 +1,12 @@
+package TestPack;
+
+public class TestCase2 {
+	public static void main(String[] args) {
+		testcasetwo();
+	}
+	
+	public static void testcasetwo(){
+		System.out.println("New test case created in branch - user1");
+	}
+
+}
