9b44df4a912a5083dff921f2b5430dd00ac939b9
SeleniumResource2 <selenium.resource2@gmail.com>
2018-07-25 11:19:17 +0530

    add new feature

--- src/TestPack/TestCase2.java
+++ src/TestPack/TestCase2.java
@@ -8,5 +8,11 @@ public class TestCase2 {
 	public static void testcasetwo(){
 		System.out.println("New test case created in branch - user1");
 	}
+	
+	public static void testcasetwo(int a, int b){
+		int c = a+b;
+		
+		System.out.println("Value of addition is : " + c);
+	}
 
 }
